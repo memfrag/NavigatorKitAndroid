44d8831189be769523ceef6a6436694e240652b2
Martin Johannesson <mjohannesson@gmail.com>
2026-07-16 22:44:55 +0200

    Release 1.0.0: bump library module versions

--- navigatorkit-compose/build.gradle.kts
+++ navigatorkit-compose/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "io.github.memfrag.navigatorkit"
-version = "0.1.0"
+version = "1.0.0"
 
 android {
     namespace = "io.github.memfrag.navigatorkit.compose"
--- navigatorkit-core/build.gradle.kts
+++ navigatorkit-core/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 group = "io.github.memfrag.navigatorkit"
-version = "0.1.0"
+version = "1.0.0"
 
 dependencies {
     // JetBrains' multiplatform Compose runtime: gives the state tree
